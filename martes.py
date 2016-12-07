39b16629c65c73d1d75ceb5d37d13e6ab527b633
Carmen Reyes <maria.reyes.h@alumnos.udg.mx>
2016-12-06 18:18:45 -0600

    creacion de clase imagen

---
+++ martes.py
@@ -0,0 +1,16 @@
+# Empezamos con git
+class Imagen():
+    def tamarindo (self):
+        pass
+    def escala_grises (self):
+        pass
+    def binarizacion (self):
+        pass
+
+class Sonido():
+    def reproducir(self):
+        pass
+    def volumen(self):
+        pass
+    def fourier(self):
+        pass
